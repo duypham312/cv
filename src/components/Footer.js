dbb7b8592df0e690baa1def13a18b28d8df8be8f
Duy <M.Duy2001@gmail.com>
2021-05-29 21:27:43 +0700

    Initial Footer

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Footer extends Component {
+    render() {
+        return (
+            <div>
+                <footer className="page-footer m-3">
+                    <div className="container">
+                        <div className="container my-auto">
+                            <div className="text-center my-auto copyright"><span>Copyright © Duy 2021</span></div>
+                        </div>
+                    </div>
+                </footer>
+            </div>
+        );
+    }
+}
+
+export default Footer;
