33ca675b5464222a9c74c5fbf8870e67e07dbc30
Duy <M.Duy2001@gmail.com>
2021-05-29 23:06:48 +0700

    Optimized

--- src/components/HireMe.js
+++ src/components/HireMe.js
@@ -11,13 +11,8 @@ class HireMe extends Component {
                                 <h2>Hire Me</h2>
                             </div>
                             <form>
-                                <div className="mb-3"><label className="form-label" htmlFor="subject">Subject</label><select className="form-select" id="subject">
-                                    <optgroup label="This is a group">
-                                        <option value={12} selected>This is item 1</option>
-                                        <option value={13}>This is item 2</option>
-                                        <option value={14}>This is item 3</option>
-                                    </optgroup>
-                                </select></div>
+                                <div className="mb-3"><label className="form-label" htmlFor="name">Your name</label><input className="form-control" type="name" id="name" /></div>
+                                <div className="mb-3"><label className="form-label" htmlFor="subject">Subject</label><input className="form-control" type="subject" id="subject" /></div>
                                 <div className="mb-3"><label className="form-label" htmlFor="email">Email</label><input className="form-control" type="email" id="email" /></div>
                                 <div className="mb-3"><label className="form-label" htmlFor="message">Message</label><textarea className="form-control" id="message" defaultValue={""} /></div>
                                 <div className="mb-3">
